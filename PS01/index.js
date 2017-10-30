d75f237487119a8b124fc8a766fa64a78d6464f3
xunchongneu <31869452+xunchongneu@users.noreply.github.com>
2017-10-30 19:20:18 -0400

    Add files via upload

--- PS01/index.js
+++ PS01/index.js
@@ -1,70 +1,64 @@
-var width = d3.select('svg').attr('width');
-var height = d3.select('svg').attr('height');
+var width = document.getElementById('svg1').clientWidth;
+var height = document.getElementById('svg1').clientHeight;
 
 var marginLeft = 100;
 var marginTop = 100;
 
 var nestedData = [];
 
-var svg = d3.select('svg')
+var svg = d3.select('#svg1')
     .append('g')
     .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
 
-//these are the size that the axes will be on the screen; set the domain values after the data loads.
-var scaleX = d3.scaleBand().rangeRound([0, 600]).padding(0.1);
-var scaleY = d3.scaleLinear().range([400, 0]);
+var svg2 = d3.select('#svg2')
+    .append('g')
+    .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
 
 
-//import the data from the .csv file
-d3.csv('./countryData_topten.csv', function(dataIn){
+var scaleX = d3.scaleBand().rangeRound([0, width-2*marginLeft]).padding(0.1);
+var scaleY = d3.scaleLinear().range([height-2*marginTop, 0]);
+var scaleY2 = d3.scaleLinear().range([height-2*marginTop, 0]);
+
+d3.csv('./xundata2.csv', function(dataIn){
 
     nestedData = d3.nest()
         .key(function(d){return d.year})
         .entries(dataIn);
 
-    var loadData = nestedData.filter(function(d){return d.key == '1987'})[0].values;
+    var loadData = nestedData.filter(function(d){return d.key ==='1'})[0].values;
 
-    // Add the x Axis
+
     svg.append("g")
         .attr('class','xaxis')
-        .attr('transform','translate(0,400)')  //move the x axis from the top of the y axis to the bottom
+        .attr('transform','translate(0,' + (height-2*marginTop) + ')')  //move the x axis from the top of the y axis to the bottom
         .call(d3.axisBottom(scaleX));
 
     svg.append("g")
         .attr('class', 'yaxis')
         .call(d3.axisLeft(scaleY));
 
-/*
-    svg.append('text')
-        .text('Weekly income by age and gender')
-        .attr('transform','translate(300, -20)')
-        .style('text-anchor','middle');
+    svg2.append("g")
+        .attr('class','xaxis')
+        .attr('transform','translate(0,' + (height-2*marginTop) + ')')  //move the x axis from the top of the y axis to the bottom
+        .call(d3.axisBottom(scaleX));
 
-    svg.append('text')
-        .text('age group')
-        .attr('transform','translate(260, 440)');
+    svg2.append("g")
+        .attr('class', 'yaxis2')
+        .call(d3.axisLeft(scaleY2));
 
-    svg.append('text')
-        .text('weekly income')
-        .attr('transform', 'translate(-50,250)rotate(270)');
 
-        */
 
-    //bind the data to the d3 selection, but don't draw it yet
-    //svg.selectAll('rect')
-    //    .data(loadData, function(d){return d;});
 
-    //call the drawPoints function below, and hand it the data2016 variable with the 2016 object array in it
+
     drawPoints(loadData);
 
 });
 
-//this function draws the actual data points as circles. It's split from the enter() command because we want to run it many times
-//without adding more circles each time.
 function drawPoints(pointData){
 
-    scaleX.domain(pointData.map(function(d){return d.countryCode;}));
-    scaleY.domain([0, d3.max(pointData.map(function(d){return +d.totalPop}))]);
+    scaleX.domain(pointData.map(function(d){return d.Stock;}));
+    scaleY.domain([0, d3.max(pointData.map(function(d){return +d.Close}))]);
+    scaleY2.domain([0, d3.max(pointData.map(function(d){return +d.Volume}))]);
 
     d3.selectAll('.xaxis')
         .call(d3.axisBottom(scaleX));
@@ -72,53 +66,123 @@ function drawPoints(pointData){
     d3.selectAll('.yaxis')
         .call(d3.axisLeft(scaleY));
 
-    //select all bars in the DOM, and bind them to the new data
-    var rects = svg.selectAll('.bars')
-        .data(pointData, function(d){return d.countryCode;});
+    d3.selectAll('.yaxis2')
+        .call(d3.axisLeft(scaleY2));
 
-    //look to see if there are any old bars that don't have keys in the new data list, and remove them.
+
+    var rects = svg.selectAll('.bars')
+        .data(pointData, function(d){return d.Stock;});
+
+
     rects.exit()
         .remove();
 
-    //update the properties of the remaining bars (as before)
+
     rects
         .transition()
-        .duration(200)
+        .duration(10)
         .attr('x',function(d){
-            return scaleX(d.countryCode);
+            return scaleX(d.Stock);
         })
         .attr('y',function(d){
-            return scaleY(d.totalPop);
+            return scaleY(d.Close);
         })
         .attr('width',function(d){
             return scaleX.bandwidth();
         })
         .attr('height',function(d){
-            return 400 - scaleY(d.totalPop);  //400 is the beginning domain value of the y axis, set above
+            return height-2*marginTop - scaleY(d.Close);
         });
 
-    //add the enter() function to make bars for any new countries in the list, and set their properties
+
     rects
         .enter()
         .append('rect')
         .attr('class','bars')
-        .attr('fill', "slategray")
+        .attr('id', function(d){return d.Stock;})
+        .attr('fill', "lightblue")
         .attr('x',function(d){
-            return scaleX(d.countryCode);
+            return scaleX(d.Stock);
         })
         .attr('y',function(d){
-            return scaleY(d.totalPop);
+            return scaleY(d.Close);
         })
         .attr('width',function(d){
             return scaleX.bandwidth();
         })
         .attr('height',function(d){
-            return 400 - scaleY(d.totalPop);  //400 is the beginning domain value of the y axis, set above
+            return height-2*marginTop - scaleY(d.Close);
+        })
+        .on('mouseover', function(d){
+            d3.select(this).attr('fill','lightblue');
+
+            currentID = d3.select(this).attr('id');
+            svg2.selectAll('#' + currentID).attr('fill','lightblue')
+        })
+        .on('mouseout', function(d){
+            d3.select(this).attr('fill','slategray');
+
+            currentID = d3.select(this).attr('id');
+            svg2.selectAll('#' + currentID).attr('fill','slategray')
         });
 
-    //take out bars for any old countries that no longer exist
-    //rects.exit()
-    //    .remove();
+
+
+    var rects2 = svg2.selectAll('.bars')
+        .data(pointData, function(d){return d.Stock;});
+
+
+    rects2.exit()
+        .remove();
+
+
+    rects2
+        .transition()
+        .duration(10)
+        .attr('x',function(d){
+            return scaleX(d.Stock);
+        })
+        .attr('y',function(d){
+            return scaleY2(d.Volume);
+        })
+        .attr('width',function(d){
+            return scaleX.bandwidth();
+        })
+        .attr('height',function(d){
+            return height-2*marginTop - scaleY2(d.Volume);
+        });
+
+
+    rects2
+        .enter()
+        .append('rect')
+        .attr('class','bars')
+        .attr('id', function(d){return d.Stock;})
+        .attr('fill', "lightblue")
+        .attr('x',function(d){
+            return scaleX(d.Stock);
+        })
+        .attr('y',function(d){
+            return scaleY2(d.Volume);
+        })
+        .attr('width',function(d){
+            return scaleX.bandwidth();
+        })
+        .attr('height',function(d){
+            return height-2*marginTop - scaleY2(d.Volume);
+        })
+        .on('mouseover', function(d){
+            d3.select(this).attr('fill','lightblue');
+
+            currentID = d3.select(this).attr('id');
+            svg.selectAll('#' + currentID).attr('fill','lightblue')
+        })
+        .on('mouseout', function(d){
+            d3.select(this).attr('fill','slategray');
+
+            currentID = d3.select(this).attr('id');
+            svg.selectAll('#' + currentID).attr('fill','slategray')
+        });
 
 
 
@@ -130,10 +194,43 @@ function updateData(selectedYear){
 }
 
 
-//this function runs when the HTML slider is moved
+
 function sliderMoved(value){
 
     newData = updateData(value);
     drawPoints(newData);
 
 }
+
+
+
+
+
+
+
+var makeLine = d3.line()
+    .x(function(d) { return scaleX(d.Stock); })
+    .y(function(d) { return scaleY(d.AdjClose); });
+
+
+
+d3.csv('./xundata2.csv', function(dataIn){
+
+    nestedData = d3.nest()
+        .key(function(d){return d.year})
+        .entries(dataIn);
+
+    var loadData = dataIn;
+
+
+    svg.append("path")
+
+        .datum(dataIn)
+        .attr("class", "line")
+        .attr("d", makeLine)
+        .attr('fill','none')
+        .attr('stroke','#00264d')
+        .attr('stroke-width',2.5);
+
+});
+
